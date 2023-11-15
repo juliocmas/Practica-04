56353888922ada5110f65f282e2a080396017f2e
julio C <144632066+juliocmas@users.noreply.github.com>
2023-11-15 16:52:59 -0500

    "Ultima actualizacion routers.cpp"

--- routers.cpp
+++ routers.cpp
@@ -1,161 +1,137 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <map>
 #include <fstream>
-#include <algorithm>
+#include <vector>
+#include <sstream>
+#include <limits.h>
 
-class Router {
-public:
-    Router(const std::string& name) : name(name) {}
+using namespace std;
 
-    void addRoute(const std::string& destination, int cost) {
-        routingTable[destination] = cost;
-    }
-
-    void removeRoute(const std::string& destination) {
-        routingTable.erase(destination);
-    }
-
-    void printRoutingTable() const {
-        std::cout << "tabla de enrutamiento para" << name << ":\n";
-        for (const auto& entry : routingTable) {
-            std::cout << "Destination: " << entry.first << ", Cost: " << entry.second << "\n";
+// Función para encontrar la distancia mínima no visitada
+int distanciaMinima(int distancias[], bool visitados[], int size) {
+    int minimo = INT_MAX;
+    int indiceMinimo = -1;
+    
+    for (int i = 0; i < size; i++) {
+        if (!visitados[i] && distancias[i] <= minimo) {
+            minimo = distancias[i];
+            indiceMinimo = i;
         }
     }
+    return indiceMinimo;
+}
 
-    void inputRoutingTable() {
-        std::cout << "Ingrese la tabla de enrutamiento para " << name << ":\n";
-        std::string destination;
-        int cost;
-        while (true) {
-            std::cout << "Introduce el destino (o 'Enter' para terminar): ";
-            std::cin >> destination;
-            if (destination == "done") {
-                break;
-            }
-            std::cout << "Enter cost: ";
-            std::cin >> cost;
-            routingTable[destination] = cost;
-        }
+// Función para imprimir el camino desde el nodo fuente hasta el nodo actual
+void imprimirCamino(int padres[], int i) {
+    if (padres[i] == -1) {
+        return;
+    }
+    imprimirCamino(padres, padres[i]);
+    cout << i + 1 << " ";
+}
+
+// Implementación del algoritmo de Dijkstra
+void dijkstra(vector<vector<int>>& router, int fuente) {
+    int size = router.size();
+    int* distancias = new int[size];
+    bool* visitados = new bool[size];
+    int* padres = new int[size];
+
+    // Inicializar arreglos de distancias, visitados y padres
+    for (int i = 0; i < size; i++) {
+        padres[i] = -1;
+        distancias[i] = INT_MAX;
+        visitados[i] = false;
     }
 
-    const std::map<std::string, int>& getRoutingTable() const {
-        return routingTable;
-    }
+    // La distancia desde la fuente a sí misma siempre es 0
+    distancias[fuente] = 0;
 
-private:
-    std::string name;
-    std::map<std::string, int> routingTable;
-};
+    // Encontrar el camino más corto para todos los nodos
+    for (int i = 0; i < size - 1; i++) {
+        int u = distanciaMinima(distancias, visitados, size);
+        visitados[u] = true;
 
-class Network {
-public:
-    void addRouter(const std::string& name) {
-        routers.emplace_back(name);
-    }
-
-    void removeRouter(const std::string& name) {
-        routers.erase(std::remove_if(routers.begin(), routers.end(), [&](const Router& router) {
-            return routerName(router) == name;
-        }), routers.end());
-    }
-
-    void loadTopologyFromFile(const std::string& filename) {
-        std::ifstream file(filename);
-        std::string source, destination;
-        int cost;
-        while (file >> source >> destination >> cost) {
-            networkTopology[source + " -> " + destination] = cost;
-            networkTopology[destination + " -> " + source] = cost;
-        }
-    }
-
-    void updateRoutingTables() {
-        for (auto& router : routers) {
-            router.updateRoutingTable(networkTopology);
-        }
-    }
-
-    void printNetworkTopology() const {
-        std::cout << "Network Topology:\n";
-        for (const auto& entry : networkTopology) {
-            std::cout << entry.first << ", Cost: " << entry.second << "\n";
-        }
-    }
-
-    void printRoutingTables() const {
-        for (const auto& router : routers) {
-            router.printRoutingTable();
-        }
-    }
-
-    Router& getRouter(int index) {
-        return routers.at(index);
-    }
-
-    std::map<std::string, int> getCostTableFromRouter(const std::string& routerName) const {
-        std::map<std::string, int> costTable;
-        Router router = findRouter(routerName);
-        if (!router.getRoutingTable().empty()) {
-            for (const auto& entry : router.getRoutingTable()) {
-                costTable[entry.first] = entry.second;
+        // Actualizar las distancias de los nodos adyacentes
+        for (int v = 0; v < size; v++) {
+            int distanciaActualizada = distancias[u] + router[u][v];
+            if (!visitados[v] && router[u][v] && distanciaActualizada < distancias[v]) {
+                padres[v] = u;
+                distancias[v] = distanciaActualizada;
             }
         }
-        return costTable;
     }
 
-    Router findRouter(const std::string& name) const {
-        auto it = std::find_if(routers.begin(), routers.end(), [&](const Router& router) {
-            return routerName(router) == name;
-        });
-
-        if (it != routers.end()) {
-            return *it;
-        } else {
-            return Router("InvalidRouter");
+    // Imprimir la distancia y el camino para cada nodo
+    cout << "Desde el nodo " << fuente + 1 << " hacia todos los nodos:" << endl;
+    for (int i = 0; i < size; i++) {
+        if (i != fuente) {
+            cout << fuente + 1 << "->" << i + 1 << "\tDistancia: " << distancias[i] << "\tCamino: " << fuente + 1 << " ";
+            imprimirCamino(padres, i);
+            cout << endl;
         }
     }
 
-private:
-    std::vector<Router> routers;
-    std::map<std::string, int> networkTopology;
-};
+    // Liberar memoria
+    delete[] distancias;
+    delete[] visitados;
+    delete[] padres;
+}
 
 int main() {
-    Network network;
+    // Solicitar al usuario que ingrese la ruta del archivo CSV
+    cout << "Ingrese la ruta donde se encuentra la matriz de adyacencia en formato CSV: ";
+    string rutaArchivo;
+    getline(cin, rutaArchivo);
 
-    int numRouters;
-    std::cout << "Ingrese la cantidad de enrutadores: ";
-    std::cin >> numRouters;
-
-    for (int i = 1; i <= numRouters; ++i) {
-        network.addRouter("Router" + std::to_string(i));
+    // Abrir el archivo
+    ifstream archivoEntrada(rutaArchivo);
+    if (!archivoEntrada.is_open()) {
+        cerr << "Error: No se pudo abrir el archivo " << rutaArchivo << endl;
+        return 1;  // Salir con código de error
     }
 
-    std::string topologyFilename;
-    std::cout << "Ingrese la ubicación del archivo de topología de red:";
-    std::cin >> topologyFilename;
-    network.loadTopologyFromFile(topologyFilename);
+    // Leer el contenido del archivo en una cadena
+    stringstream buffer;
+    buffer << archivoEntrada.rdbuf();
+    string contenidoArchivo = buffer.str();
 
-    for (int i = 0; i < numRouters; ++i) {
-        Router& router = network.getRouter(i);
-        router.inputRoutingTable();
+    // Procesar la cadena para construir la matriz
+    stringstream streamMatriz(contenidoArchivo);
+    vector<vector<int>> router;
+
+    string linea;
+    while (getline(streamMatriz, linea)) {
+        vector<int> fila;
+        stringstream streamLinea(linea);
+        int valor;
+        char coma;
+
+        while (streamLinea >> valor) {
+            fila.push_back(valor);
+            streamLinea >> coma;  // Leer la coma
+        }
+
+        router.push_back(fila);
     }
 
-    network.updateRoutingTables();
+    // Cerrar el archivo
+    archivoEntrada.close();
 
-    network.printNetworkTopology();
-    network.printRoutingTables();
+    // Obtener el tamaño de la matriz
+    int size = router.size();
 
-    std::string selectedRouter;
-    std::cout << "Ingrese el nombre del enrutador para ver la tabla de costos: ";
-    std::cin >> selectedRouter;
+    // Verificar que la matriz sea cuadrada
+    for (const auto& fila : router) {
+        if (fila.size() != size) {
+            cerr << "Error: La matriz no es cuadrada." << endl;
+            return 1;  // Salir con código de error
+        }
+    }
 
-    std::map<std::string, int> costTable = network.getCostTableFromRouter(selectedRouter);
-    std::cout << "tabla de costos de " << selectedRouter << ":\n";
-    for (const auto& entry : costTable) {
-        std::cout << "Destino: " << entry.first << ", Cost: " << entry.second << "\n";
+    // Ejecutar el algoritmo de Dijkstra para cada nodo como fuente
+    for (int fuente = 0; fuente < size; ++fuente) {
+        dijkstra(router, fuente);
+        cout << "-----------------------------------" << endl;
     }
 
     return 0;
